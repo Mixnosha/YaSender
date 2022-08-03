621a6bd7fe3280bb21e00670acfab7fae8235c70
mixnosha <xmixho@gmail.com>
2022-08-03 17:21:48 +0300

    models

--- send/admin.py
+++ send/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from send.models import *
+
+admin.site.register(Email)
+admin.site.register(GroupEmail)
+admin.site.register(CustomizeUser)
--- send/models.py
+++ send/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Email(models.Model):
+    Type = [
+        ("sender", "sender"), ("recipient", "recipient")
+    ]
+    type = models.CharField(choices=Type, max_length=255)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.email
+
+
+class GroupEmail(models.Model):
+    name_group = models.CharField(max_length=255)
+    email = models.ForeignKey('Email', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name_group
+
+class CustomizeUser(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    groups = models.ForeignKey('GroupEmail', on_delete=models.CASCADE)
+    email = models.ForeignKey('Email', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user
